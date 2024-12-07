1d6f1a92394785470a1d721b8b79a4739955a8ea
Leuza Akter <47984104+zerokanake@users.noreply.github.com>
2024-12-07 12:33:18 +0000

    conversion of datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accound_Id = 123342
+let account_email = "leu45@.com"
+var accont_passward = "1234"
+account_city = " mirpur"
+
+account_email = "mdjd@NFJ.com"
+accont_passward = "9475"
+account_city = "dhanondy"
+
+console.table([accound_Id, account_email, accont_passward, account_city])
---
+++ 01_basics/03_onversionOperation.js
@@ -0,0 +1,35 @@
+let score = "30abc"
+
+//const {score} = req.body
+
+let valueInNumber = Number(score)
+
+//console.log(typeof score)
+//console.log(typeof (score));
+
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber);
+
+
+// "30" => 30
+// "30abc" => NaN (not a number)
+// true => 1 ; false => 0
+// undifine => NaN
+// Null => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+/* 
+    "" => false
+    "red" => true
+*/
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber)
